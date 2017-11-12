6c6c379c5b9054690fb178c7ed7c05d1ebf60400
Olivia Nicholls <31240787+olivianicholls@users.noreply.github.com>
2017-11-12 13:41:02 +0000

    Add comments

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
   let result = 0;
   totaldiv.text('0');
 
+  // Button click animation
   $('.num-btn').on('click', function() {
     $(this).animate({'top': '2px'}, 60);
     $(this).animate({'top': '0'}, 60);
@@ -17,27 +18,31 @@ $(document).ready(function() {
     $(this).animate({'top': '2px'}, 60);
     $(this).animate({'top': '0'}, 60);
   });
+
+  // Get button text and make into number
   $(".number").click(function() {
     numberString += $(this).text();
     totaldiv.text(numberString);
     numberInt = parseFloat(numberString);
   });
 
+  // Get operator text
   $(".operator").not('#clear', '#clearall').click(function() {
     operator = $(this).text();
     totaldiv.text(numberString + '' + operator);
-    arrNum.push(numberInt)
-    numberString = '';
-	operatorArr.push(operator);
-
+    arrNum.push(numberInt) // push number to array
+    numberString = ''; // clear for next number
+    operatorArr.push(operator);
   });
 
+  // Clear last number
   $('#clear').click(function() {
     numberString = '';
     numberInt = 0;
     totaldiv.text(arrNum[0] + '' + operator);
   });
 
+  // Clear all numbers and operators
   $('#clearall').click(function() {
     numberString = '';
     numberInt = 0;
@@ -46,6 +51,7 @@ $(document).ready(function() {
     totaldiv.text('0');
   });
 
+  // Calctulation functions
   $('#equals').click(function() {
     arrNum.push(numberInt);
     let n1 = arrNum[0];
@@ -68,11 +74,12 @@ $(document).ready(function() {
         result = n1 * n2;
         result.toString(10);
         break;
-    }
-    totaldiv.text(result);
-	arrNum = [];
-	operatorArr = [];
-	numberString = result
-	numberInt = parseFloat(result);
-  });
+      }
+      totaldiv.text(result);
+      // Allow user to continue calculation with result
+      arrNum = [];
+      operatorArr = [];
+      numberString = result
+      numberInt = parseFloat(result);
+    });
 });
